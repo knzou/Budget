ae2cb33577262457e53da741b90d1398f16d4299
Ken Zou <kenzou@Kens-Mac-mini.local>
2021-10-04 15:21:14 -0400

    fix

--- server.go
+++ server.go
@@ -1,35 +1,36 @@
 package main
 
 import (
-    	"context"
-    	"net"
+	"context"
+	"net"
 
-    	"github.com/knzou/BudgetCalculatorService/proto"
-    	"google.golang.org/grpc"
-        "google.golang.org/grpc/reflection"
+	"github.com/knzou/BudgetCalculatorService/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
+
 type server struct{}
 
 func main() {
-    listener, err := net.Listen("tcp", ":4040")
-    if err != nil {
-        panic(err)
-    }
-    srv := grpc.NewServer()
-    // add our services into the grpc server
-    proto.RegisterAddServiceServer(srv, &server{})
-    // reflection will set up serializing and deserializing
-    reflection.Register(srv)
+	listener, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		panic(err)
+	}
+	srv := grpc.NewServer()
+	// add our services into the grpc server
+	proto.RegisterAddServiceServer(srv, &server{})
+	// reflection will set up serializing and deserializing
+	reflection.Register(srv)
 
-    if e := srv.Serve(listener); e != nil {
-        panic(err)
-    }
+	if e := srv.Serve(listener); e != nil {
+		panic(err)
+	}
 }
 
 func (s *server) GetCategories(ctx context.Context, request *proto.Request) (*proto.GetCategoriesResponse, error) {
-	return &proto.GetCategoriesResponse{CatId: "id", Name: "name", TypeId: "typeId"}, nil
+	return &proto.GetCategoriesResponse{CatId: 1, Name: "name", TypeId: 2}, nil
 }
 
 func (s *server) GetTransactions(ctx context.Context, request *proto.Request) (*proto.GetTransactionsResponse, error) {
-	return &proto.GetTransactionsResponse{TranId: "id", CatId: "id", Amount: "amount"}, nil
+	return &proto.GetTransactionsResponse{TranId: 1, CatId: 1, Amount: 100}, nil
 }
